ba595c876bf8da16de2bf67cbb71081c42d65c9b
Alexandr Toropov <toropovsite@yandex.ru>
2022-05-06 00:32:17 +0300

    Добавил основные каталоги в конфиг

--- config.php
+++ config.php
@@ -1,1 +1,5 @@
 <?php
+//корневая директория проекта
+const ROOT_DIR = __DIR__;
+//директория с шаблонами
+const TPL_DIR = __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR;
