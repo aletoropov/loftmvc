b2b0b8a280bd478ae4caf8f95a9b17ae4d0c2f38
Alexandr Toropov <toropovsite@yandex.ru>
2022-05-06 00:19:44 +0300

    Добавил PHPDoc

--- src/AbstractController.php
+++ src/AbstractController.php
@@ -2,10 +2,16 @@
 
 namespace Core;
 
+
 abstract class AbstractController
 {
     abstract function actionIndex();
 
+    /**
+     * @param string $url
+     * @return mixed
+     * @throws RedirectException
+     */
     protected function redirect(string $url)
     {
         throw new RedirectException($url);
