52ccaac9574798415c0ed7ca795f0f7325b0304c
Alexandr Toropov <toropovsite@yandex.ru>
2022-05-06 00:29:19 +0300

    Подключение конфига и автолодера composer

--- html/index.php
+++ html/index.php
@@ -1,10 +1,15 @@
 <?php
-require_once "../vendor/autoload.php";
-require_once "../config.php";
+require_once '../config.php';
+require_once ROOT_DIR . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use Core\Application;
 
 $parts = parse_url($_SERVER['REQUEST_URI']);
 
 $app = new Application();
-$app->run();
+
+try {
+    $app->run();
+} catch (\Core\RouteException $e) {
+    echo $e->getMessage();
+}
