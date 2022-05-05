fde1f7387785c1ad5b45a14819ede7fd123038fe
Alexandr Toropov <toropoviste@yandex.ru>
2022-05-05 03:54:09 +0300

    Базовый роутинг

--- html/index.php
+++ html/index.php
@@ -3,4 +3,18 @@ include "../vendor/autoload.php";
 
 use Core\Application;
 
-$app = new Application();
+$parts = parse_url($_SERVER['REQUEST_URI']);
+
+switch ($parts['path']) {
+    case '/':
+        echo 'index';
+        break;
+    case '/user/login':
+        echo 'user login';
+        break;
+    case '/user/register':
+        echo 'user/register';
+        break;
+    default:
+        http_response_code(404);
+}
