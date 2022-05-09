9093d26619a8f9fe500d7609900a2553bd9b9e19
Alexandr Toropov <toropovsite@yandex.ru>
2022-05-09 20:50:02 +0300

    Данные для подключения к базе данных

--- config.php
+++ config.php
@@ -3,3 +3,8 @@
 const ROOT_DIR = __DIR__;
 //директория с шаблонами
 const TPL_DIR = __DIR__ . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR;
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'loftmvc';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
