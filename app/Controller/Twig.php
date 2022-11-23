58ac6e13f9e65394a83a67c2d173f0c7a24f1aa9
Alexandr Toropov <toropovsite@yandex.ru>
2022-11-23 07:32:55 +0300

    Искользования шаблонизатора Twig

--- app/Controller/Twig.php
+++ app/Controller/Twig.php
@@ -7,9 +7,10 @@ class Twig extends \Core\AbstractController
 
     public function actionIndex()
     {
-        $loader = new \Twig\Loader\ArrayLoader([
-            'index' => 'Hello {{ name }}!',
-        ]);
+        $loader = new \Twig\Loader\FilesystemLoader('../templates');
         $twig = new \Twig\Environment($loader);
+
+        $template = $twig->load('index.html');
+        echo $template->render(['name' => 'Alexandr']);
     }
 }
